b9f546921812d6d28c8864f154a92ff4609d3cf1
xanderkjoller <xander@kjollers.dk>
2025-10-08 11:43:09 +0200

    hdfg

--- src/main/java/org/example/kino/restcontroller/ShowingRestController.java
+++ src/main/java/org/example/kino/restcontroller/ShowingRestController.java
@@ -1,13 +1,32 @@
 package org.example.kino.restcontroller;
 
+import org.example.kino.model.Show;
+import org.example.kino.repositories.ShowRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
 
 public class ShowingRestController {
 
-    @GetMapping("/Showings")
-    private String showings(@RequestParam("movieId")int movieId)
+    @Autowired
+    ShowRepository showRepository;
+
+    @GetMapping("/Showings/{movieId}")
+    private List<Show> showings(@PathVariable("movieId")int movieId)
     {
-        return "Showings";
+        List<Show> showings = showRepository.findAll();
+        List<Show> filteredShowings = new ArrayList<>();
+        for (Show i : showings) {
+            if(i.getMovie().getMovieID() == movieId){
+                filteredShowings.add(i);
+            }
+        }
+        return filteredShowings;
     }
 }
