777f516938d542940f39185ccdb3c0fba4d2c942
alex <albo0003@kea.stud.dk>
2025-10-10 10:13:06 +0200

    fracments header

--- src/main/java/org/example/kino/controller/SeatReservationController.java
+++ src/main/java/org/example/kino/controller/SeatReservationController.java
@@ -7,6 +7,7 @@ import org.example.kino.repositories.SeatReservationRepository;
 import org.example.kino.repositories.ShowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -14,10 +15,14 @@ import java.util.List;
 @Controller
 public class SeatReservationController {
 
+    @Autowired
+    ShowRepository showRepository;
+
     @GetMapping("/SeatReservation")
-    public String seatReservation(HttpServletRequest request, HttpSession session)  {
+    public String seatReservation(HttpServletRequest request, HttpSession session, Model model)  {
         int showID = Integer.parseInt(request.getParameter("showID"));
         session.setAttribute("showID", showID);
+        model.addAttribute("show", showRepository.findById(showID).get());
         return "seatReservation";
     }
 
